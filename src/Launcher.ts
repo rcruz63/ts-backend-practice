e95ecb7bf8a23c81d8644932b2d6caeb9eb8ff07
Raul de la Cruz <rpublico@gmail.com>
2022-11-12 08:23:09 +0000

    New: Node Server v2

---
+++ src/Launcher.ts
@@ -0,0 +1,14 @@
+import { Server } from "./Server";
+
+
+class Launcher {
+    
+    private server: Server = new Server();
+    
+    public launchApp(){
+        this.server.startServer();
+    }
+    
+}
+
+new Launcher().launchApp();
---
+++ src/Server.ts
@@ -0,0 +1,18 @@
+import { createServer} from 'http';
+import { IncomingMessage, ServerResponse } from 'node:http';
+
+export class Server {
+    
+    public startServer(){
+        
+        createServer(
+            (req: IncomingMessage, res: ServerResponse)=>{
+                console.log(`Got request from ${req.headers['user-agent']} for ${req.url}`)
+                res.write('Hello from TS server!')
+                res.end();
+            }
+        ).listen(8080);
+        console.log('Server started!')
+        
+    }
+}
